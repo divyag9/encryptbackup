1479359d7b6c470598b8cea7668d1c9262e17904
SGPD\dmuppaneni <divya.gorantla@safeguardproperties.com>
2017-02-16 17:24:34 -0600

    Adding more tests

--- packages/encrypt/encrypt_test.go
+++ packages/encrypt/encrypt_test.go
@@ -7,8 +7,22 @@ var directoryPaths = []struct {
 	target string
 }{
 	{
-		"/cmd/server/",
-		"/cmd/client",
+		"cmd\\server",
+		"target",
+	},
+	{
+		"source",
+		"test.txt",
+	},
+}
+
+var pgpKeys = []struct {
+	sgp string
+	mid string
+}{
+	{
+		"D://Users//dmuppaneni//Documents//moran.key",
+		"D://Users//dmuppaneni//Documents//midland.key",
 	},
 }
 
@@ -16,7 +30,16 @@ func TestCheckSourceAndTargetDirectories(t *testing.T) {
 	for _, path := range directoryPaths {
 		err := checkSourceAndTargetDirectories(path.source, path.target)
 		if err == nil {
-			t.Errorf("Expected: Invalid paths")
+			t.Errorf("Expected: invalid source or target directory")
+		}
+	}
+}
+
+func TestCreateEntityList(t *testing.T) {
+	for _, pgpKeys := range pgpKeys {
+		_, err := createEntityList(pgpKeys.mid, pgpKeys.sgp)
+		if err != nil {
+			t.Errorf("Expected: no error creating the entitylist")
 		}
 	}
 }
